9a598fd2350335e4a514dcdb139da3d8d358e919
bsilverriverd <edl1111@naver.com>
2021-08-24 13:52:05 +0900

    Listing 2-6

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 // Listing 2-3: Adding code to generate a random number
 // Listing 2-4: Handling the possible return values of comparing two numbers
+// Listing 2-5: Ignoring a non-number guess and asking for another guess instead of crashing the program
 use std::io ;
 use std::cmp::Ordering ;
 use rand::Rng ;
@@ -8,9 +9,7 @@ fn main() {
 	println!("Guess the number!") ;
 
 	let secret_number = rand::thread_rng().gen_range(1..101) ; // (1..=100)
-
-	println!("The secret number is: {}", secret_number) ;
-
+	
 	loop {
 		println!("Please input your guess.") ;
 	
@@ -20,7 +19,10 @@ fn main() {
 			.read_line(&mut guess)
 			.expect("Failed to read line") ;
 	
-		let guess: u32 = guess.trim().parse().expect("Please type a number!") ;
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		} ;
 	
 		println!("You guessed: {}", guess) ;
 	
