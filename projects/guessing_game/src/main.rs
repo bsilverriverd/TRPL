8abac52be80080d2f957fc178f1ebe98b4eb02d3
bsilverriverd <edl1111@naver.com>
2021-08-24 13:26:36 +0900

    Listing 2-3

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
-use std::io;
+// Listing 2-3: Adding code to generate a random number
+use std::io ;
+use rand::Rng ;
 
 fn main() {
 	println!("Guess the number!") ;
+
+	let secret_number = rand::thread_rng().gen_range(1..101) ; // (1..=100)
+
+	println!("The secret number is: {}", secret_number) ;
 
 	println!("Please input your guess.") ;
 
